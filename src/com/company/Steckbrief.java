8b3c5b035de37c786801653f1426160ebc968ab4
Christopher Voglgsang <chris@christophersmbp.mschool-ad.muenchen.musin.de>
2020-10-19 13:04:03 +0200

    update

--- src/com/company/Steckbrief.java
+++ src/com/company/Steckbrief.java
@@ -2,24 +2,25 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class Steckbrief {
 
     public static void main(String[] args) {
-        System.out.println("Name eingeben:");
+        System.out.print("Name eingeben:");
         Scanner name = new Scanner(System.in);
         String n = name.next();
 
-        System.out.println("Alter eingeben:");
+        System.out.print("Alter eingeben:");
         Scanner alter = new Scanner(System.in);
         int a = alter.nextInt();
 
-        System.out.println("Geschlecht m/w:");
+        System.out.print("Geschlecht m/w:");
         Scanner geschlecht = new Scanner(System.in);
         String g = geschlecht.next();
 
         System.out.println("Name: " + n);
         System.out.println("Alter: " + a);
         System.out.println("Geschlecht: " + g);
+
 
 
     }
